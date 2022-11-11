107bf8430d8154381c7c23ec34c2c48f0d776c8e
froi <froilanaquino1@gmail.com>
2022-11-11 13:19:39 +0200

    deployment

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-let stars = document.getElementById('stars');
-let meteorid = document.getElementById('meteorid');
-let rocket = document.getElementById('rocket');
-let text = document.getElementById('text');
-let button = document.getElementById('button');
+$(document).ready( e => {
+    let stars = document.getElementById('stars');
+    let meteorid = document.getElementById('meteorid');
+    let rocket = document.getElementById('rocket');
+    let text = document.getElementById('text');
 
-window.addEventListener('scroll', function(){
-    let value = window.scrollY;
-    stars.style.left = value * 0.25 + 'px';
-    rocket.style.top = value * -0.5 + 'px';
-    text.style.marginBottom = value * 1 + 'px';
-    button.style.marginBottom = value * 1 + 'px';
+    $(window).scroll(x => {
+        let value = window.scrollY;
+        stars.style.top = value * 0.20 + 'px';
+        rocket.style.top = value * -0.5 + 'px';
+        text.style.marginBottom = value * 1 + 'px';
+    });
 });
